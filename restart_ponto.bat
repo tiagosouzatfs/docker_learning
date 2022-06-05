5c7f4dab595308785bacb61bb2581349c4a83f44
Tiago Felipe de Souza <54643787+tiagosouzatfs@users.noreply.github.com>
2022-06-05 11:37:18 -0300

    Delete restart_ponto.bat

--- restart_ponto.bat
+++
@@ -1,8 +0,0 @@
-echo novo terminal para reinício do ponto
-whoami
-TASKLIST /FI "IMAGENAME eq java.exe"
-cd C:/Windows/system32
-TASKKILL /F /IM java.exe /T
-cd C:/Users/PONTO - GCTI/Downloads
-java -jar -Dspring.profiles.active=prod leitor-0.0.1-SNAPSHOT.jar
-pause
