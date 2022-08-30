df871ea884ee0230217e0142165be9493b36d102
Tiago Felipe de Souza <54643787+tiagosouzatfs@users.noreply.github.com>
2022-08-29 23:15:00 -0300

    Finalização do curso

---
+++ yaml/app.py
@@ -0,0 +1,11 @@
+import yaml
+
+if __name__ == '__main__':
+    
+    stream = open('test.yaml', 'r')
+    dictionary = yaml.safe_load(stream)
+
+    for key, value in dictionary.items():
+        print(key + ' : ' + str(value))
+
+    stream.close()
